70327ebd19985345f5605de2ef9444ba5ac6a739
José Lucas <josluc2016@gmail.com>
2024-07-18 00:24:28 -0300

    fix: update

--- src/prompt_messages.py
+++ src/prompt_messages.py
@@ -1,10 +1,35 @@
 from InquirerPy import prompt
 
+from flask import current_app
+
+from models.access_pattern import Access_pattern
+
 
 class Prompt_messages:
 
     def __init__(self) -> None:
         self.selected_stack = ''
+
+    def add_new_stack(self):
+        add_new_stack_prompt = [
+            {
+                "type": "input",
+                "message": "What is the stack name?",
+                "name": "stack_name",
+            },
+            {
+                "type": "input",
+                "message": "What is the stack description?",
+                "name": "stack_description",
+            }
+        ]
+
+        stack_prompt = prompt(add_new_stack_prompt)
+        access_pattern_db = Access_pattern(
+            stack_prompt['stack_name'], stack_prompt['stack_description'])
+
+        with current_app.app_context():
+            access_pattern_db.add()
 
     def stack(self):
         default = [
@@ -18,25 +43,5 @@ class Prompt_messages:
 
         self.selected_stack = prompt(default)
 
-        return self.selected_stack['stack']
-
-    def add_new_stack(self):
         if self.selected_stack['stack'] == 'Or add new stack':
-            add_new_stack_prompt = [
-                {
-                    "type": "input",
-                    "message": "What is the stack name?",
-                    "name": "stack_name",
-                },
-                {
-                    "type": "input",
-                    "message": "What is the stack description?",
-                    "name": "stack_description",
-                }
-            ]
-
-            stack_prompt = prompt(add_new_stack_prompt)
-
-    def caller(self):
-        self.stack()
-        self.add_new_stack()
+            self.add_new_stack()
