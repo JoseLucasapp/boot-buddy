1bc0bddad22ac4cbd5462d08dc50c878b63dc881
José Lucas <josluc2016@gmail.com>
2024-07-19 01:11:21 -0300

    feat: add and get functions

--- models/access_links.py
+++ models/access_links.py
@@ -1,12 +1,15 @@
 from database.database import db
+from json import dumps
+from sqlalchemy import text
 
 
 class Access_links(db.Model):
-    id = db.Column('id', db.BigInteger, primary_key=True, autoincrement=True)
+    __tablename__ = 'access_links'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     link = db.Column('link', db.String)
     link_name = db.Column('link_name', db.String)
     access_apps_id = db.Column(
-        'access_apps_id', db.BigInteger, db.ForeignKey('access_apps.id'))
+        'access_apps_id', db.Integer, db.ForeignKey('access_apps.id'))
 
     def __init__(self, link, link_name, access_apps_id):
         self.link = link
@@ -18,3 +21,14 @@ class Access_links(db.Model):
             return {"id": self.id, "link": self.link, "link_name": self.link_name, "access_apps_id": self.access_apps_id}
         else:
             return {"col": getattr(self, col) for col in columns}
+
+    def add(self):
+        access_links = Access_links(
+            link=self.link, link_name=self.link_name, access_apps_id=self.access_apps_id)
+        db.session.add(access_links)
+        db.session.commit()
+
+    def get(self):
+        result = db.session.execute(
+            text('SELECT * FROM access_links')).fetchall()
+        return dumps([dict(row._mapping) for row in result])
