300ed3097b72af75411b599c4a13809e6b5a976c
José Lucas <josluc2016@gmail.com>
2024-07-27 19:39:24 -0300

    fix: current app

--- src/system_settings.py
+++ src/system_settings.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from flask import current_app
 
 
 class System_settings:
@@ -8,3 +9,12 @@ class System_settings:
 
     def quit_app():
         sys.exit(0)
+
+    def call_command(command):
+        os.system(command)
+
+    def start_file(file_path):
+        os.startfile(file_path)
+
+    def get_current_app():
+        return current_app
